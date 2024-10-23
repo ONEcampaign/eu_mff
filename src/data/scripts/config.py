756303a9064fec235052897e633168fc8b0a7b37
Jorge Rivera <jorge.rivera@one.org>
2024-10-23 09:02:02 +0200

    refactor structure

--- src/data/scripts/config.py
+++ src/data/scripts/config.py
@@ -4,8 +4,8 @@ from pathlib import Path
 class Paths:
     """Class to store the paths to the data and output folders."""
 
-    project = Path(__file__).resolve().parent.parent.parent.parent.parent
+    project = Path(__file__).resolve().parent.parent.parent.parent
     raw_data = project / "raw_data"
-    app = project / "data_app"
+    app = project
     app_data = app / "src" / "data"
     scripts = app_data / "scripts"
